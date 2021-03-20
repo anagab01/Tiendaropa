35c00ff20a5c7a1f71ea8e0b12ab885bdd16ffc0
ANA GABRIELA MEJIA MARTINEZ <agmejiam@unah.hn>
2021-03-20 13:36:48 -0600

    agregando segundo formulario

--- proyectoTiendadeRopa/TiendadeRopa.BL/ProductosBL.cs
+++ proyectoTiendadeRopa/TiendadeRopa.BL/ProductosBL.cs
@@ -15,10 +15,23 @@ namespace TiendadeRopa.BL
             _contexto = new Contexto();
             ListadeProductos = new List<Producto>();
         }
+
+
         public List<Producto> ObtenerProductos()
         {
             ListadeProductos = _contexto.Productos
                 .Include("Categoria")
+                .OrderBy(r => r.Categoria.Descripcion)
+                .ThenBy(r => r.Descripcion)
+                .ToList();
+            return ListadeProductos;
+        }
+        public List<Producto> ObtenerProductosActivos()
+        {
+            ListadeProductos = _contexto.Productos
+                .Include("Categoria")
+                .Where(r => r.Activo == true)
+                .OrderBy(r => r.Descripcion)
                 .ToList();
             return ListadeProductos;
         }
--- proyectoTiendadeRopa/TiendadeRopa.Win/Program.cs
+++ proyectoTiendadeRopa/TiendadeRopa.Win/Program.cs
@@ -16,7 +16,7 @@ namespace TiendadeRopa.Win
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new Form2());
         }
     }
 }
